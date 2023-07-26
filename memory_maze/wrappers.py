89a74694f609ca22310976c740c48ecbcd84077d
sangbin <sbpark0611@kaist.ac.kr>
2023-07-26 16:24:09 +0900

    1

--- memory_maze/wrappers.py
+++ memory_maze/wrappers.py
@@ -5,7 +5,7 @@ from typing import Any, Dict, List
 import dm_env
 import numpy as np
 from dm_env import specs
-
+from gymnasium import space
 
 class Wrapper(dm_env.Environment):
     """Base class for dm_env.Environment wrapper."""
@@ -200,7 +200,7 @@ class DiscreteActionSetWrapper(Wrapper):
 
     def __init__(self, env: dm_env.Environment, action_space: List[np.ndarray]):
         super().__init__(env)
-        self.action_space = np.array(action_space)
+        self.action_space = spaces.Discrete(action_space)
 
     def action_spec(self):
         return specs.DiscreteArray(len(self.action_space))
