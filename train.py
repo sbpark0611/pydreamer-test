8b22494e802bf3bdcf68633311f30319413f6f39
sangbin <sbpark0611@kaist.ac.kr>
2023-08-03 02:20:37 +0900

    1

--- train.py
+++ train.py
@@ -338,6 +338,8 @@ def drawGraph(conf, test_len):
             obs, reward, done, inf = env.step(action)
             if reward > prev_reward:
                 steps_per_task[i].append(inf["episode_steps"] - prev_steps)
+                prev_steps = inf["episode_steps"]
+                prev_reward = reward
 
     # calculate mean steps per task and log by mlflow
     mean_steps_per_task = []
